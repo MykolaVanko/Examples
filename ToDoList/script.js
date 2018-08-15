c06c0db81054cf1806afa2daa34640ca78ab3dd6
MykolaVanko <vankomykola@gmail.com>
2018-08-15 16:38:44 +0300

    use li for creating to do list

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -3,21 +3,16 @@
 
    $('.toggle').on('click', function () {
        $('.task_display').slideToggle();
-       $('#minus, #plus').toggle();
+       $('.clear').val('');
+   });$('.cancel').on('click', function () {
+       $('.task_display').slideToggle();
        $('.clear').val('');
    });
 
-                  /* watch task*/
-   $('.list').on('click','.list_task', function () {
-       $('.desc_wrapper',this).slideToggle('hidden');
-       $('.list_task-seting', this).toggleClass('btn_visibility');
-       $('.list_task-seting, .list_task-name input, .description, .map').on('click', function (e) {
-           e.stopPropagation();
-       });
-   });
-
                   /* delete task*/
-
+    $('ul').on('click', '.geo', function () {
+       $('.map_task', this).slideToggle()
+    });
 
 
 
@@ -25,27 +20,19 @@
 
     $('.add').on('click', function () {
 
-        let setingBtn = $('<div class="list_task-seting btn_visibility"></div>')
-            .append('<img class="done" src="img/if_success_1646004.png" height="24"/>')
-            .append('<img class="edit" src="img/if_Compose_1891025.png" height="24"/>')
-            .append('<img class="delete" src="img/if_error_1646012.png" height="24"/>');
+        let taskElements = $('<li class="task_list"></li>')
+            .append('<p class="task_list_name">name</p>' +
+                '<p class="task_list_date">Date</p>' +
+                '<p class="task_list_description">description</p>' +
+                '<div class="map_task"></div>' +
+                '<img class="mrg-10 geo" src="img/if_53-Location_2123937.png" height="24"/>' +
+                '<img class="mrg-10 done" src="img/if_success_1646004.png" height="24"/>' +
+                '<img class="mrg-10 edit" src="img/if_Compose_1891025.png" height="24"/>' +
+                '<img class="mrg-10 delete" src="img/if_error_1646012.png" height="24"/>');
 
-        let description = $('<div class="hidden desc_wrapper"></div>')
-            .append('<textarea readonly class="description"></textarea>')
-            .append('<div class="map"></div>');
+        $('ul').append(taskElements);
 
-        let listName = $('<div class="list_task-name"></div>')
-            .append('<input class="taskName" type="text" readonly> ' +
-                '<input type="date" readonly> ')
-            .append(setingBtn);
-
-        let taskElements = $('<div class="list_task new_list_task"></div>')
-            .append(listName)
-            .append(description);
-
-        $('.list').append(taskElements);
-
-        $('.taskName', this).val( $('#taskName').val() );
+        let taskName = $('#taskName').val();
         let taskDate = $('#taskDate').val();
         let taskText = $('#taskText').val();
 
